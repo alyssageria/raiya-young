57c954b2c8d9a5dd12459cd1d7ead9cf02998332
Alyssa Geria <alyssageria@gmail.com>
2024-02-27 14:40:32 -0800

    mobile view for homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -18,16 +18,16 @@ function Home() {
                     </svg>
                 </div>
             </div>
-            <div className="about bg-cream h-[50vh]">
+            <div className="about bg-cream xs:h-screen lg:max-h-[50vh]">
                 {/* <div className="flex justify-center">
                     <img src={aboutScript} className="about-script"></img>
                 </div> */}
-                <div className="grid grid-cols-2">
-                    <div className="flex justify-center ml-20">
-                        <img src={raiya} className="raiya"></img>
+                <div className="lg:grid grid-cols-2">
+                    <div className="flex justify-center lg:ml-20">
+                        <img src={raiya} className="raiya xs:m-5 lg:m-auto xs:h-64 lg:h-96"></img>
                     </div>
-                    <div className="text-center flex flex-col items-center mr-20">
-                        <img src={aboutScript} className="h-40"></img>
+                    <div className="text-center flex flex-col items-center xs:m-5 lg:mr-20">
+                        <img src={aboutScript} className="xs:h-32 lg:h-40"></img>
                         <p className="font-montserrat text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel nisi nec ante condimentum efficitur a et mi. Nam et mauris sem. Donec eget justo ante. Sed vel finibus libero. Quisque ex dolor, iaculis eu gravida et, porttitor nec ligula. Duis diam nisi, ornare ut mi a, elementum vestibulum lacus. Phasellus at auctor eros. Nunc nibh lacus, aliquam varius justo ac, ultrices iaculis sapien. Quisque tempus, purus quis mollis lobortis, lorem leo fringilla nunc, vel dapibus augue erat vitae nulla. Aliquam tristique bibendum quam, et rhoncus nulla convallis facilisis. Sed at varius neque. Etiam aliquam lectus et bibendum fringilla.</p>
                     </div>
                 </div>
@@ -38,12 +38,12 @@ function Home() {
                         viewBox="0 0 1440 319"><path fill="#edeae6" fill-opacity="1" d="M0,160L120,144C240,128,480,96,720,101.3C960,107,1200,149,1320,170.7L1440,192L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
                         </path>
                     </svg>
-                    <div className="grid grid-cols-2">
-                        <div className="text-center flex flex-col items-center ml-20">
-                            <img src={speakingScript} className="h-40"></img>
+                    <div className="lg:grid grid-cols-2">
+                        <div className="text-center flex flex-col items-center lg:ml-20 xs:m-5">
+                            <img src={speakingScript} className="xs:h-32 lg:h-40"></img>
                             <p className="font-montserrat text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel nisi nec ante condimentum efficitur a et mi. Nam et mauris sem. Donec eget justo ante. Sed vel finibus libero. Quisque ex dolor, iaculis eu gravida et, porttitor nec ligula. Duis diam nisi, ornare ut mi a, elementum vestibulum lacus. Phasellus at auctor eros. Nunc nibh lacus, aliquam varius justo ac, ultrices iaculis sapien. Quisque tempus, purus quis mollis lobortis, lorem leo fringilla nunc, vel dapibus augue erat vitae nulla. Aliquam tristique bibendum quam, et rhoncus nulla convallis facilisis. Sed at varius neque. Etiam aliquam lectus et bibendum fringilla.</p>
                         </div>
-                        <div className="flex justify-center ml-20">
+                        <div className="flex justify-center lg:ml-20">
 
                         </div>
                     </div>
